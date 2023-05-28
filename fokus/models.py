bfb6fe94aa04136b1120970774d039d761c56ab0
gbhisma <bhismagregorius@gmail.com>
2023-05-28 18:00:18 +0700

    added analysis model

--- fokus/models.py
+++ fokus/models.py
@@ -1,3 +1,15 @@
+import datetime
+
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Analysis(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    time = models.DateTimeField(default=datetime.datetime.now())
+    description = models.CharField(max_length=200)
+    session_length = models.DurationField()
+    focus_length = models.DurationField()
+
+
