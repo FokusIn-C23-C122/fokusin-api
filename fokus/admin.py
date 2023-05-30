914db475a164b026df6ae3cf6fdf3a905a264dae
gbhisma <bhismagregorius@gmail.com>
2023-05-31 01:18:22 +0700

    feat: models declaration

--- fokus/admin.py
+++ fokus/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from fokus.models import Analysis, Image
+
 # Register your models here.
+admin.site.register(Analysis)
+admin.site.register(Image)
--- fokus/models.py
+++ fokus/models.py
@@ -1,15 +1,26 @@
 import datetime
 
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
 # Create your models here.
 class Analysis(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    time = models.DateTimeField(default=datetime.datetime.now())
+    time_started = models.DateTimeField(default=datetime.datetime.now())
     description = models.CharField(max_length=200)
     session_length = models.DurationField()
     focus_length = models.DurationField()
 
+
+class Image(models.Model):
+    analysis_session = models.ForeignKey(Analysis, on_delete=models.CASCADE)
+    timestamp = models.DurationField(default=datetime.datetime.now())
+    image = models.ImageField(upload_to="analysis/")
+    confidence = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+    status = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.analysis_session}_{self.timestamp}"
 
